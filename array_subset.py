822f5f1e2bc9d18f0ca091076109a5b0e3a7d559
Sanjana Kumari <sanjanakumari46649@gmail.com>
2026-02-20 14:09:15 +0530

    Delete array_subset.py

--- array_subset.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-def is_subset(a, b):
-    count_a = Counter(a)
-    count_b = Counter(b)
-    for key in count_b:
-        if count_b[key] > count_a.get(key, 0):
-            return False
-    return True
